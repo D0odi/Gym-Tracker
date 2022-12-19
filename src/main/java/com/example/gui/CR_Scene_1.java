cb8d40496b8cbb40f070e85e08bbd191d65e8fdd
DooDik <natekinlev@gmail.com>
2022-12-18 19:32:09 -0700

    0

--- src/main/java/com/example/gui/CR_Scene_1.java
+++ src/main/java/com/example/gui/CR_Scene_1.java
@@ -28,21 +28,25 @@ public class CR_Scene_1 {
 
     @FXML
     public void toScene2(ActionEvent e) throws IOException {
+        try {
+            stage = (Stage) ((Node) e.getSource()).getScene().getWindow(); // not to create multiple stages
 
-        stage = (Stage)((Node)e.getSource()).getScene().getWindow(); // not to create multiple stages
-
-        FileChooser fc = new FileChooser();
-        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-        data.setFile(fc.showOpenDialog(stage));
-
-
-        root = FXMLLoader.load(getClass().getResource("Scene_2.fxml"));
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+            FileChooser fc = new FileChooser();
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+            data.setFile(fc.showOpenDialog(stage));
 
 
-        data.read_data();
+            root = FXMLLoader.load(getClass().getResource("Scene_2.fxml"));
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+
+            data.read_data();
+        }
+        catch (Exception ex) {
+            data.loadError();
+        }
     }
     public void toScene2_newFile(ActionEvent e) throws IOException {
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
